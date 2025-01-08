af273f3a3c44f8859df096d45042ae569281084b
Samir Shah <samirshah@Samirs-MacBook-Pro.local>
2025-01-07 23:52:36 -0500

    updated public key

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'YOUR_SERVICE_ID', // Replace with your EmailJS service ID
-  TEMPLATE_ID: 'YOUR_TEMPLATE_ID', // Replace with your EmailJS template ID
-  PUBLIC_KEY: 'YOUR_PUBLIC_KEY', // Replace with your EmailJS public key
+  SERVICE_ID: 'service_rfx8d7h', // Replace with your EmailJS service ID from step 2
+  TEMPLATE_ID: 'template_0wsbzw9', // Replace with your template ID from step 5
+  PUBLIC_KEY: 'public_3GF2uHpYEQckod6x1', // Replace witI h your public key from step 5
 };
