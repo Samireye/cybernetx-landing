5dd7261bf42a110cd9d53275ae4ddb307a69fef0
Samir Shah <samirshah@Samirs-MacBook-Pro.local>
2025-01-07 23:22:01 -0500

    fix: add favicon and improve routing

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,22 @@ export default defineConfig({
   ],
   optimizeDeps: {
     include: ['@mdx-js/react']
+  },
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          blog: [
+            './src/blog/components/BlogList.jsx',
+            './src/blog/components/BlogPost.jsx',
+            './src/blog/components/NewsletterSubscribe.jsx',
+            './src/blog/components/SocialShare.jsx'
+          ]
+        }
+      }
+    }
   }
 });
